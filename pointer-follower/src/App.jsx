e25096d5f4ab9ce134e3503f73a84617e6d928c9
Jesus Carrasco <Jesuscarrascoyel.mur@gmail.com>
2024-03-19 11:51:58 +0100

    Pointer Follower

--- pointer-follower/src/App.jsx
+++ pointer-follower/src/App.jsx
@@ -1,33 +1,77 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const FollowMouse = () => {
+
+
+  //UseState de enabled inicado en false
+  const [enabled, setEnabled] = useState(false)
+  //UseState de position inicado en (0,0)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+
+  useEffect(() => {
+    /*Evento para detectar la ubicación del cursor.(Dentro del useEffect para controlar cuando se suscribe al evento,
+      no cada vez que se ejecute el componente, si no cuando cambie el valor de enabled)*/
+    const handleMove = (event) => {
+      //Guardo las cordenadas en las que se encuentra el cursor.
+      const { clientX, clientY } = event
+      //Cambio la posicion de pointer-follower a la posición del puntero pasandole la posicion al css del puntero.
+      setPosition({ x: clientX, y: clientY })
+    }
+    //Solo inicio el evento seguir puntero si enabled es true cuando pare de serlo elimino el envento.
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    /*-CLEANUP
+      El return se ejecuta cuando el componente se desmonta/desaparece y cuando cambia la dependencia ([enabled]). 
+      Aprovecho para pasar al return una funcion anónima que limpia el efecto (lo borra).*/
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+  return (
+    <main>
+      <div style={
+        {
+          position: 'absolute',
+          backgroundColor: '#09f',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`
+
+
+        }
+      }></div>
+      <h3>Proyecto</h3>
+      <button onClick={() =>
+        //Al pulsar el boton cambio el valor de enabled al contrario.
+        setEnabled(!enabled)
+      }>{enabled ? 'Desactivar' : 'Activar'} seguir puntero</button >
+    </main >
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  //Desmonta el evento FollowMouse.
+  const [mounted, setMounted] = useState(false)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {mounted && <FollowMouse />}
+      <button onClick={() => {
+        setMounted(!mounted)
+      }}>{mounted ? 'Desmontar' : 'Montar'} FollowMouse component.</button>
     </>
   )
 }
