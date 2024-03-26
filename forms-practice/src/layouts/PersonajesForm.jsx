fab402c100bfd9cfaf64c872e784dc5831712abf
Jesus Carrasco <Jesuscarrascoyel.mur@gmail.com>
2024-03-26 20:13:55 +0100

    practiceForms commit

--- forms-practice/src/layouts/PersonajesForm.jsx
+++ forms-practice/src/layouts/PersonajesForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 /*
    Porps: name ,faction, profession, race
 
@@ -6,21 +6,35 @@ import React from 'react'
    */
 export const PersonajesForm = () => {
 
+    const [character, setCharacter] = useState([]);
     //Metodo para prevenir la recarga de la página.
     const submitForm = (event) => {
         event.preventDefault();
-        console.log(event.target[0].value)
+        //Guardo el character con las props de los inputs obtenidos del evento
+        const character = {
+            name: event.target[0].value,
+            faction: event.target[1].value,
+            profession: event.target[2].value,
+            race: event.target[3].value
+
+        }
+        setCharacter(character)
+        console.log(character)
+
+        document.querySelectorAll(".char-form-input").forEach((input) => (
+            input.value = ""
+        ))
     }
 
     return (
         <>
-
+            <h3></h3>
             <form onSubmit={submitForm} className='char-form'>
-
                 <input type="text" className='char-form-input' placeholder='Name' name='name' />
                 <input type="text" className='char-form-input' placeholder='Faction' name='faction' />
                 <input type="text" className='char-form-input' placeholder='Profession' name='profession' />
                 <input type="text" className='char-form-input' placeholder='Race' name='race' />
+                <br />
                 <button type='submit'>Guardar</button>
             </form>
         </>
